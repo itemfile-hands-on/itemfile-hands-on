b64a6bf7c6abbb2b47003135b0e98fe834fda4b0
Keith Langreck <Keith.Langreck@walmart.com>
2018-04-03 08:14:35 -0500

    First Commit

---
+++ src/IfLogic.java
@@ -0,0 +1,25 @@
+/**
+ * Code a program method called logic that accepts an Integer values and returns a String
+ *  Logic 
+ *   - if divisible by 5 evenly, then return "Very"
+ *   - if divisible by 7 evenly, then return "Good"
+ *   - if divisible by both 5 and 7 evenly, then return "Very Good"
+ *  Test - Pass in:
+ *   - 14 = Good
+ *   - 25 = Very
+ *   - 35 = Very Good
+ *   - 44 = 
+ */
+public class IfLogic {
+
+	public static void main(String[] args) {
+		//System.out.println( logic( 14 ) );
+		//System.out.println( logic( 25 ) );
+		//System.out.println( logic( 35 ) );
+		//System.out.println( logic( 44 ) );
+	}
+	
+
+	
+
+}
